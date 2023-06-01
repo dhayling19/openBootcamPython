1075e8935c53660d5cf849a8477e02f85e623d5d
Dhayling <dhay19rojas@gmail.com>
2023-06-01 15:39:35 -0300

    ejercicio 1 de ficheros

---
+++ archivitos.py
@@ -0,0 +1,18 @@
+def main():
+
+    with open('mifichero.txt', 'w') as fichero:
+        fichero.write('Hola estoy escribiendo aqui y tengo hambre')
+    
+
+if __name__ == '__main__':
+    main()
+
+
+ 
+
+
+
+
+
+
+
